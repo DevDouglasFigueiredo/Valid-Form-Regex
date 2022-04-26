ffea59ee08be677a33f50125441b3f38e57ae074
Douglas Figueiredo <devdouglasfigueiredo@gmail.com>
2022-04-26 16:52:00 -0300

    update file

---
+++ js/validacao.js
@@ -0,0 +1,42 @@
+export function valida(input){
+    const tipoDeinput = input.dataset.tipo
+
+    if (validadores[tipoDeInput]){
+        validadores[tipoDeInput] (input)
+
+    }
+
+    if(input.validity.valid){
+        input.parentElement.classList.remove('input-container--invalido')
+
+    } else {
+        input.parentElement.classList.add('input-container--invalido')
+    }
+
+
+}
+
+const validadores = {
+    dataNascimento: input => validaDataNascimento(input)
+}
+
+function validaDataNascimento (input){
+    const dataRecebida = new Date(input.value)
+    let mensagem = "";
+
+    if (!maiorQue18(dataRecebida)){
+    mensagem = "Você deve ser maior que 18 anos para se cadastrar"
+}
+
+
+    input.setCustomValidity(mensagem)
+
+}
+
+function maiorQue18 (data){
+    const dataAtual = new Date()
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate())
+
+    return dataMais18 <= dataAtual
+
+}
